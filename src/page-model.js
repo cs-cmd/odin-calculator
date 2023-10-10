36e69da25d3399c0f6a2809e727069f3186f62d0
Ray <rayoneill.97@gmail.com>
2023-10-10 14:35:20 -0400

    Completed rough draft of calculator

--- src/page-model.js
+++ src/page-model.js
@@ -1,75 +1,126 @@
+let vals = [ null, null, null ];
+function clearArray() {
+    vals[0] = null;
+    vals[1] = null; 
+    vals[2] = null;
+}
+
+let screen = document.getElementsByClassName('calc-screen')[0];
+let screenHeader = document.getElementsByClassName('calc-header')[0];
+
 function initButtons() {
-    let addButton = document.getElementById("add-button");
-    let subtractButton = document.getElementById("subtract-button");
-    let multiplyButton = document.getElementById("multiply-button");
-    let divideButton = document.getElementById("divide-button");
+    let calcButtons = document.getElementsByClassName("calc-button");
 
-    addButton.addEventListener("click", (e) => {
-        performFunc(add);
-    });
+    for (let i = 0; i < calcButtons.length; ++i) {
+        calcButtons[i].addEventListener("click", (e) => {
+            let value = e.target.innerText;
+            let type = e.target.getAttribute("ro-type");
 
-    subtractButton.addEventListener("click", (e) => {
-        performFunc(subtract);
-    });
-
-    multiplyButton.addEventListener("click", (e) => {
-        performFunc(multiply);
-    });
-
-    divideButton.addEventListener("click", (e) => {
-        performFunc(divide);
-    })
+            console.log(value);
+            console.log(type);
+            handleButtonClick(type, value);
+        })
+    }
 }
 
-function performFunc(func) {
-    let values = getValues();
-    
-    if(values == null) {
-        // error
+function operate(num1, operand, num2) {
+    let func;
+
+    switch (operand) {
+        case "+":
+            func = add;
+            break;
+        case "-":
+            func = subtract;
+            break;
+        case "*": 
+            func = multiply;
+            break;
+        case "/":
+            func = divide;
+            break;
+        default:
+            showError("Error in computation...");
+            return;
+    }
+
+    let result = func(num1, num2);
+
+    showResult(result);
+}
+
+function handleButtonClick(type, value) {
+    switch (type) {
+        case "decimal":
+        case "number":
+            handleNumberClick(value);
+            break;
+        case "operand":
+            handleOperandClick(value);
+            break;
+        case "equals":
+            handleEqualsClick(value);
+            operate(vals[0], vals[1], vals[2]);
+            clearArray();
+            break;
+        case "clear": 
+            clearCalcScreen();
+            break;
+        default:
+            showError("Error in processing...");
+            break;
+    }
+}
+
+function handleNumberClick(value) {
+    let text = screen.innerText;
+
+    if (text.length >= 54) {
+        showError("Number too long...");
+        return;
+
+    }
+
+    screen.innerText = text + value;
+}
+
+function handleOperandClick(value) {
+    let num1 = parseFloat(screen.innerText);
+
+    if (isNaN(num1)) {
+        showError("Number is invalid...");
         return;
     }
 
-    let numOne = values[0];
-    let numTwo = values[1];
+    vals[0] = num1;
+    vals[1] = value;
 
-    let result = func(numOne, numTwo);
+    let displayText = screen.innerText + ` ${value}`;
+    screenHeader.innerText = displayText;
 
-    showOutput(result);
+    screen.innerText = '';
 }
 
-function getValues() {
-    let numOneInput = document.getElementById("num1-input");
-    let numTwoInput = document.getElementById("num2-input");
+function handleEqualsClick(value) {
+    num2 = parseFloat(screen.innerText);
 
-    let numOne = parseInt(numOneInput.value);
-    let numTwo = parseInt(numTwoInput.value);
-
-    if (isNaN(numOne)) {
-        showError("First number invalid");
-        return;
+    if(isNaN(num2)) {
+        showError("Number is invalid...");
     }
 
-    if (isNaN(numTwo)) {
-        showError("Second number invalid");
-        return;
-    }
-
-    return [numOne, numTwo ];
+    vals[2] = num2;
 }
 
+function showResult(result) {
+    screenHeader.innerText = '';
+    screen.innerText = result;
+}
 function showError(errMsg) {
-    let errorTag = document.getElementById("error-output");
-
-    let errorMessage = `ERROR: ${errMsg}`;
-
-    errorTag.innerText = errorMessage;
+    let errTag = document.getElementById("error-output");
+    errTag.innerText = errMsg;
 }
 
-function showOutput(output) {
-    let outputTag = document.getElementById("result-output");
-
-    outputTag.innerText = `Result is: ${output}`;
+function clearCalcScreen() {
+    screen.innerText = '';
 }
-
-
 initButtons();
